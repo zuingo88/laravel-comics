74ce0906a824f82c153fc1a7f3d137ce06bd9e22
valerio sgura <valeriosgura88@gmail.com>
2021-05-18 09:16:42 +0200

    working on footer

--- comics/resources/views/components/footer.blade.php
+++ comics/resources/views/components/footer.blade.php
@@ -1,3 +1,26 @@
 <footer>
-    <h2>footer</h2>
+    <h2>Dc Comics</h2>
+    <ul>
+        @foreach ($dc_comics as $item)
+            <li>{{ $item }}</li>
+        @endforeach
+    </ul>
+    {{-- <h2>Shop</h2>
+    <ul>
+        @foreach ($shop as $item)
+            <li>{{ $item }}</li>
+        @endforeach
+    </ul>
+    <h2>Dc</h2>
+    <ul>
+        @foreach ($dc as $item)
+            <li>{{ $item }}</li>
+        @endforeach
+    </ul>--}}
+    {{-- <h2>Sites</h2>
+    <ul>
+        @foreach ($sites as $item)
+            <li>{{ $item }}</li>
+        @endforeach
+    </ul>  --}}
 </footer>
--- comics/resources/views/pages/home.blade.php
+++ comics/resources/views/pages/home.blade.php
@@ -11,7 +11,50 @@
             <div class="container relative">
                 <h2 class="absolute">Current series</h2>
 
+                <div class="card_container flex_wrap space_bet">
 
+                    @foreach ($data as $item)
+                        <div class="card flex_col">
+                            <div class="thumb">
+                                <img src="{{ $item['thumb'] }}" alt="">
+                            </div>
+                            <p>{{ $item['series'] }}</p>
+                        </div>
+                    @endforeach
+
+                </div>
+
+                <div class="more flex_center">
+                    <button class="btn">load more</button>
+                </div>
+            </div>
+        </section>
+
+        <section id="big_stripe">
+            <div class="container flex space_bet align_cen">
+
+                <ul class="flex space_bet align_cen">
+                    <li class="block flex align_cen">
+                        <img src="{{ asset('/storage/assets/buy-comics-digital-comics.png') }}" alt="">
+                        <a href=""><span>Digital Comics</span></a>
+                    </li>
+                    <li class="block flex align_cen">
+                        <img src="{{ asset('/storage/assets/buy-comics-merchandise.png') }}" alt="">
+                        <a href=""><span>Dc Merchandise</span></a>
+                    </li>
+                    <li class="block flex align_cen">
+                        <img src="{{ asset('/storage/assets/buy-comics-subscriptions.png') }}" alt="">
+                        <a href=""><span>Subscription</span></a>
+                    </li>
+                    <li class="block flex align_cen">
+                        <img src="{{ asset('/storage/assets/buy-comics-shop-locator.png') }}" alt="">
+                        <a href=""><span>Comic Shop Locator</span></a>
+                    </li>
+                    <li class="block flex align_cen">
+                        <img src="{{ asset('/storage/assets/buy-dc-power-visa.svg') }}" alt="">
+                        <a href=""><span>Dc Power Visa</span></a>
+                    </li>
+                </ul>
             </div>
         </section>
     </main>
